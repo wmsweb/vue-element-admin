5efe8a3c110bf0224a85c9c2fc15e88be61bea65
wumingsheng <wu_mingsheng@126.com>
2020-03-10 10:09:36 +0800

    permission.js实现方式解读,添加中文注释

--- src/permission.js
+++ src/permission.js
@@ -6,31 +6,31 @@ import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
 
-NProgress.configure({ showSpinner: false }) // NProgress Configuration
+NProgress.configure({ showSpinner: false }) // 进度条配置,取消右上角旋转的环
 
-const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
+const whiteList = ['/login', '/auth-redirect'] // 白名单
 
 router.beforeEach(async(to, from, next) => {
-  // start progress bar
+  // 开始进度条
   NProgress.start()
 
-  // set page title
+  // 获取to页面的title
   document.title = getPageTitle(to.meta.title)
 
-  // determine whether the user has logged in
+  // 获取token
   const hasToken = getToken()
 
-  if (hasToken) {
+  if (hasToken) { // 有token
     if (to.path === '/login') {
       // if is logged in, redirect to the home page
       next({ path: '/' })
       NProgress.done()
     } else {
-      // determine whether the user has obtained his permission roles through getInfo
+      // 获取当前用户的角色列表 hasRoles标记是否生成总的路由,静态路由+动态路由(过滤出有权限的)
       const hasRoles = store.getters.roles && store.getters.roles.length > 0
-      if (hasRoles) {
+      if (hasRoles) { // 有角色直接放行
         next()
-      } else {
+      } else { // 登录后第一次进来,还没有roles表明也没有生成总的有权限的路由,下面的逻辑是生成roles,过滤出所有有权限的路由,合并到总路由里面去
         try {
           // get user info
           // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
@@ -39,7 +39,7 @@ router.beforeEach(async(to, from, next) => {
           // generate accessible routes map based on roles
           const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
 
-          // dynamically add accessible routes
+          // 动态添加这个角色的路由(把所有动态路由里面有角色的路由过滤出来,添加到所有路由里面),生成左侧sidebar
           router.addRoutes(accessRoutes)
 
           // hack method to ensure that addRoutes is complete
@@ -55,13 +55,13 @@ router.beforeEach(async(to, from, next) => {
       }
     }
   } else {
-    /* has no token*/
+    /* 没有token */
 
-    if (whiteList.indexOf(to.path) !== -1) {
-      // in the free login whitelist, go directly
+    if (whiteList.indexOf(to.path) !== -1) { // 在白名单
+      // 在白名单,不需要权限,直接放行
       next()
     } else {
-      // other pages that do not have permission to access are redirected to the login page.
+      // 不在白名单,重定向到login页面
       next(`/login?redirect=${to.path}`)
       NProgress.done()
     }
@@ -69,6 +69,6 @@ router.beforeEach(async(to, from, next) => {
 })
 
 router.afterEach(() => {
-  // finish progress bar
+  // 进度条结束
   NProgress.done()
 })
